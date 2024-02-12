b0a8d2fedfd1a445a50e7ee23b0f5418016202ad
Samiii777 <58442200+Samiii777@users.noreply.github.com>
2024-02-12 15:39:06 -0500

    Add Docker installation

---
+++ Linux/install_docker.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Check if the script is running with root privileges
+if [ "$(id -u)" -ne 0 ]; then
+    echo "Please run this script with root privileges (sudo)."
+    exit 1
+fi
+
+# Before you can install Docker Engine, you need to uninstall any conflicting packages.
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+ 
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# Install Docker packages
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Verify that the Docker Engine installation is successful by running the hello-world image.
+sudo docker run hello-world
+
+# Source: https://docs.docker.com/engine/install/ubuntu/
