5d09ab4d4385de729bd79f8067e4a81cd66380aa
Samiii777 <58442200+Samiii777@users.noreply.github.com>
2024-02-16 10:41:31 -0500

    Removed Previous Folders

--- Linux/ROCm 6.0.2/install_pyTorch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Install Pytorch
-wget https://repo.radeon.com/rocm/manylinux/rocm-rel-6.0.2/torch-2.1.2+rocm6.0-cp310-cp310-linux_x86_64.whl
-wget https://repo.radeon.com/rocm/manylinux/rocm-rel-6.0.2/torchvision-0.16.1+rocm6.0-cp310-cp310-linux_x86_64.whl
-
-pip3 install --force-reinstall torch-2.1.2+rocm6.0-cp310-cp310-linux_x86_64.whl torchvision-0.16.1+rocm6.0-cp310-cp310-linux_x86_64.whl
-
