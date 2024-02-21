aab106f6574b50490a01ad2882605c2834be039b
Samiii777 <samankhatir@gmail.com>
2024-02-20 22:24:37 -0500

    Add OnnxRuntime Installation

---
+++ Linux/ROCm-6.0.2/install_OnnxRT.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install Onnx Runtime
+apt install migraphx
+apt install half
+pip3 install https://repo.radeon.com/rocm/manylinux/rocm-rel-6.0.2/onnxruntime_rocm-inference-1.17.0-cp310-cp310-linux_x86_64.whl
+
+
