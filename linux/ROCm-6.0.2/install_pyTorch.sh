a702f08dde32a2c2008c9d0dcb13eb26dc0b3f65
Samiii777 <58442200+Samiii777@users.noreply.github.com>
2024-02-26 13:12:04 -0500

    Add Pytorch check

--- linux/ROCm-6.0.2/install_pyTorch.sh
+++ linux/ROCm-6.0.2/install_pyTorch.sh
@@ -10,3 +10,5 @@ pip3 install --force-reinstall torch-2.1.2+rocm6.0-cp310-cp310-linux_x86_64.whl 
 
 # Clean up downloaded files
 rm torch-2.1.2+rocm6.0-cp310-cp310-linux_x86_64.whl torchvision-0.16.1+rocm6.0-cp310-cp310-linux_x86_64.whl
+
+python3 ../tests/check_pytorch.py
