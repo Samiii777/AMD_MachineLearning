12555c6f8a859433f4eb2264358284a493926e4d
Samiii777 <samankhatir@gmail.com>
2024-02-25 23:54:43 -0500

    update cmake install

--- linux/install_cmake.sh
+++ linux/install_cmake.sh
@@ -1,8 +1,20 @@
-#Replace the version with your desired one.
+#!/bin/bash
+
+#Change the cmake version, with your desired one
+URL_BASE="https://github.com/Kitware/CMake/releases/download/"
+CMAKE_VERSION="3.28.3"
+
+sudo apt-get update
+sudo apt-get install libssl-dev
 
 sudo apt autoremove cmake
 sudo apt clean
-wget https://github.com/Kitware/CMake/releases/download/v3.26.3/cmake-3.26.3.tar.gz
-tar xvf cmake-3.26.3.tar.gz
-cd cmake-3.26.3
+wget "${URL_BASE}v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}.tar.gz"
+tar xvf cmake-${CMAKE_VERSION}.tar.gz
+cd cmake-${CMAKE_VERSION}
 ./bootstrap && make && sudo make install
+
+# Remove downloaded file and extracted folder
+cd ..
+rm -f cmake-${CMAKE_VERSION}.tar.gz
+sudo rm -rf cmake-${CMAKE_VERSION}
