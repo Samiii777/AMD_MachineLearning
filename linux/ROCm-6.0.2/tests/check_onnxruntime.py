30b72242e961ade0ffd5bbff011ab6be6ef32272
Samiii777 <samankhatir@gmail.com>
2024-02-26 12:52:39 -0500

    Moved tests folder

--- linux/ROCm-6.0.2/tests/check_onnxruntime.py
+++
@@ -1,9 +0,0 @@
-import onnxruntime as ort
-
-providers = ort.get_available_providers()
-if 'MIGraphXExecutionProvider' in providers:
-    print("Onnx runtime has been installed successfully with the MIGraphX provider")
-else:
-    print("ERROR: Onnx runtime is not installed with the MIGraphX provider")
-
-
