e210a035d05e7cae851722e9aa14ed07d1af60ba
Samiii777 <samankhatir@gmail.com>
2024-02-26 19:44:51 -0500

    update amd_info

--- linux/scripts/amd_info.sh
+++ linux/scripts/amd_info.sh
@@ -1,11 +1,48 @@
 #!/bin/bash
 
-# Extract ROCm version installed on the system
-rocm_version=$(apt-cache show rocm-libs | grep Version | grep -oP 'Version: \K\d+\.\d+\.\d+')
+#!/bin/bash
 
-# Check if the version is non-empty
-if [ -n "$rocm_version" ]; then
-    echo "ROCm version installed: $rocm_version"
-else
-    echo "Unable to determine ROCm version."
-fi
+# Function to extract GPU version
+getGfxVersion() {
+    local gfxVersion=$(rocminfo | awk '/Agent 2/,/^  Name:/ {if ($1 == "Name:") print $2}')
+
+    # Check and print GPU version
+    if [ -n "$gfxVersion" ]; then
+        echo "gfxVersion: $gfxVersion"
+    else
+        echo "Unable to determine GPU version."
+    fi
+}
+
+# Function to extract Marketing Name
+getMarketingName() {
+    local marketingName=$(rocminfo | awk '/Agent 2/,/^  Marketing Name:/ {if ($1 == "Marketing" && $2 == "Name:") {$1=""; print $0}}' | sed 's/^[ \t]*Name:[ \t]*//')
+    # Check and print Marketing Name
+    if [ -n "$marketingName" ]; then
+        echo "Marketing Name: $marketingName"
+    else
+        echo "Unable to determine Marketing Name."
+    fi
+}
+
+# Function to extract ROCm version
+getRocmVersion() {
+    local rocmVersion=$(apt-cache show rocm-libs | grep Version | grep -oP 'Version: \K\d+\.\d+\.\d+')
+
+    # Check and print ROCm version
+    if [ -n "$rocmVersion" ]; then
+        echo "ROCm version installed: $rocmVersion"
+    else
+        echo "Unable to determine ROCm version."
+    fi
+}
+
+# Function to check and print all information
+checkAndPrintInfo() {
+    getRocmVersion
+    getGfxVersion
+    getMarketingName
+}
+
+# Call the function to check and print all information
+checkAndPrintInfo
