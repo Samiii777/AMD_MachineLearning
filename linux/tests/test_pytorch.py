3f4c6801eb986e7ca8c756ec3ee16560a2c5f188
Samiii777 <58442200+Samiii777@users.noreply.github.com>
2024-03-18 16:07:03 -0400

    Add pytorch version check

--- linux/tests/test_pytorch.py
+++ linux/tests/test_pytorch.py
@@ -1,7 +1,13 @@
 import torch
+import torchvision
 
 # Check if GPU is available through pytorch
 if torch.cuda.is_available():
     print("GPU is available.")
+    print("Installed version of PyTorch:")
+    print(torch.__version__)
+    print("Installed version of TorchVision:")
+    print(torchvision.__version__)
 else:
     print("ERROR: GPU is not available. It is advised to restart your system if you haven't done so yet and try again after the restart.")
+    print("Also make sure you have added the user to the render and video group.")
