0db109c76068f535c870c5f0fb777a6fa987a9d9
Samiii777 <samankhatir@gmail.com>
2024-06-17 15:11:36 -0400

    Refine Onnxruntime test

--- linux/tests/test_onnxruntime.py
+++ linux/tests/test_onnxruntime.py
@@ -1,9 +1,17 @@
 import onnxruntime as ort
 
 providers = ort.get_available_providers()
-if 'MIGraphXExecutionProvider' in providers:
-    print("Onnx runtime has been installed successfully with the MIGraphX provider")
+
+if 'ROCMExecutionProvider' in providers and 'MIGraphXExecutionProvider' in providers:
+    print("Onnx runtime has been installed successfully with both ROCm and MIGraphX providers.")
 else:
-    print("ERROR: Onnx runtime is not installed with the MIGraphX provider")
+    missing_providers = []
+    if 'ROCMExecutionProvider' not in providers:
+        missing_providers.append('ROCMExecutionProvider')
+    if 'MIGraphXExecutionProvider' not in providers:
+        missing_providers.append('MIGraphXExecutionProvider')
 
-
+    if missing_providers:
+        print(f"ERROR: Onnx runtime is not installed with the following provider(s): {', '.join(missing_providers)}")
+    else:
+        print("ERROR: Onnx runtime is not installed with either ROCm or MIGraphX provider.")
