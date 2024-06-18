0f4d7ab47882475c2911097b92507fd7aeb578c1
Samiii777 <58442200+Samiii777@users.noreply.github.com>
2024-06-18 17:13:07 -0400

    Add Triton Version Check

--- linux/tests/test_pytorch.py
+++ linux/tests/test_pytorch.py
@@ -1,5 +1,6 @@
 import torch
 import torchvision
+import triton
 
 # Check if GPU is available through pytorch
 if torch.cuda.is_available():
@@ -8,6 +9,8 @@ if torch.cuda.is_available():
     print(torch.__version__)
     print("Installed version of TorchVision:")
     print(torchvision.__version__)
+    print("Installed version of Triton:")
+    print(triton.__version__)
 else:
     print("ERROR: GPU is not available. It is advised to restart your system if you haven't done so yet and try again after the restart.")
     print("Also make sure you have added the user to the render and video group.")
