a6ff060ee7541464d500ec656c542b3e247ffd3d
Samiii777 <58442200+Samiii777@users.noreply.github.com>
2024-07-31 17:19:37 -0400

    Fix Link for onnxruntime

--- linux/rocm/rocm-6.1.3/install_onnxruntime.sh
+++ linux/rocm/rocm-6.1.3/install_onnxruntime.sh
@@ -4,4 +4,4 @@
 sudo apt install migraphx -y
 sudo apt install half -y
 pip3 uninstall onnxruntime-rocm -y
-pip3 install https://repo.radeon.com/rocm/manylinux/rocm-rel-6.1.3/onnxruntime_rocm-inference-1.17.0-cp310-cp310-linux_x86_64.whl
+pip3 install https://repo.radeon.com/rocm/manylinux/rocm-rel-6.1.3/onnxruntime_rocm-1.17.0-cp310-cp310-linux_x86_64.whl
