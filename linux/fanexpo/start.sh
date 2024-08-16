e2812651e6887ba05fd043c366965cf6d68361af
Samiii777 <58442200+Samiii777@users.noreply.github.com>
2024-08-16 15:11:48 -0400

    some changes

--- linux/fanexpo/start.sh
+++ linux/fanexpo/start.sh
@@ -16,12 +16,19 @@ tmux split-window -v -t $SESSION_NAME
 tmux split-window -h -t $SESSION_NAME
 
 # Send commands to each pane
-tmux send-keys -t $SESSION_NAME:0.0 "source ~/miniconda3/etc/profile.d/conda.sh && conda activate llava && export PYTHONPATH=/home/$USER/Documents/LLaVA/:$PYTHONPATH && python3 llava/serve/controller.py" C-m
+tmux send-keys -t $SESSION_NAME:0.0 "source ~/miniconda3/etc/profile.d/conda.sh && conda activate llava && export PYTHONPATH=~/src/llava-amd-radeon-demo:$PYTHONPATH && python3 ~/src/llava-amd-radeon-demo/llava/serve/controller.py" C-m
 
-tmux send-keys -t $SESSION_NAME:0.1 "sleep 5 && source ~/miniconda3/etc/profile.d/conda.sh && conda activate llava && export PYTHONPATH=/home/$USER/Documents/LLaVA/:$PYTHONPATH && python3 llava/serve/model_worker.py --model-path liuhaotian/llava-v1.6-34b" C-m
+tmux send-keys -t $SESSION_NAME:0.1 "sleep 5 && source ~/miniconda3/etc/profile.d/conda.sh && conda activate llava && export PYTHONPATH=~/src/llava-amd-radeon-demo:$PYTHONPATH && python3 ~/src/llava-amd-radeon-demo/llava/serve/model_worker.py --model-path liuhaotian/llava-v1.6-34b" C-m
 
-tmux send-keys -t $SESSION_NAME:0.2 "sleep 40 && source ~/miniconda3/etc/profile.d/conda.sh && conda activate llava && export PYTHONPATH=/home/$USER/Documents/LLaVA/:$PYTHONPATH && python3 llava/serve/gradio_web_server.py" C-m
+tmux send-keys -t $SESSION_NAME:0.2 "sleep 40 && source ~/miniconda3/etc/profile.d/conda.sh && conda activate llava && export PYTHONPATH=~/src/llava-amd-radeon-demo:$PYTHONPATH && python3 ~/src/llava-amd-radeon-demo/llava/serve/gradio_web_server.py" C-m
+
+sleep 50
+firefox http://localhost:7860
 
 # Attach to the tmux session
-tmux attach -t $SESSION_NAME
+tmux attach-session -t $SESSION_NAME
+
+
+
+                                                                                                                                                                                                                                              │
 
