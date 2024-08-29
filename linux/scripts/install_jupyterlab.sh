45c4767bfa34377130b2aa666f4e01a2985e7460
Samiii777 <samankhatir@gmail.com>
2024-08-29 16:12:16 -0400

    remove sudo condition

--- linux/scripts/install_jupyterlab.sh
+++ linux/scripts/install_jupyterlab.sh
@@ -3,16 +3,12 @@
 current_dir=$(dirname "$0")
 
 # Check if the script is running with root privileges
-if [ "$(id -u)" -ne 0 ]; then
-    echo "Please run this script with root privileges (sudo)."
-    exit 1
-fi
 
 # Install pip3
-apt install -y python3-pip 
+sudo apt install -y python3-pip 
 #Install Jupyter-lab
-snap install jupyter
-apt install -y jupyter-core
+sudo snap install jupyter
+sudoapt install -y jupyter-core
 
 pip3 install jupyterlab
 
