8ffa30215d5d26b0a82dfe95e2bfaf6953397eb0
Samiii777 <samankhatir@gmail.com>
2025-02-25 10:44:14 -0500

    uninstall onnxruntime and tensorflow to avoid conflicting with rocm one

--- linux/rocm/rocm-6.3.2/install_onnxruntime.sh
+++ linux/rocm/rocm-6.3.2/install_onnxruntime.sh
@@ -3,6 +3,6 @@
 # Install Onnx Runtime
 sudo apt install migraphx -y
 sudo apt install half -y
-pip3 uninstall onnxruntime-rocm -y
+pip3 uninstall onnxruntime-rocm onnxruntime -y
 pip3 install onnxruntime-rocm -f https://repo.radeon.com/rocm/manylinux/rocm-rel-6.3.2/
 
