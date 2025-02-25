21d8b938f513f1545f33d35fda2d77b42bb2f11a
Samiii777 <58442200+Samiii777@users.noreply.github.com>
2025-02-25 11:26:03 -0500

    fix typo

--- linux/rocm/rocm-6.3.2/install_amd_driver_with_rocm_on_ubuntu.sh
+++ linux/rocm/rocm-6.3.2/install_amd_driver_with_rocm_on_ubuntu.sh
@@ -10,7 +10,7 @@ fi
 wget -r -np -nH --cut-dirs=4 -P /tmp/ https://repo.radeon.com/amdgpu-install/6.3.2/ubuntu/jammy/amdgpu-install_6.3.60302-1_all.deb
 
 # Install AMD Driver Package
-apt-get install -y /tmp/amdgpu-install_6.3.60302-1_all.debb
+apt-get install -y /tmp/amdgpu-install_6.3.60302-1_all.deb
 
 # Install AMD Driver for Graphics and ROCm
 ## replace graphics with workstation if you are using a Workstation Graphich Card
